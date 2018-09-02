df3221d7619579faf51bc4df1fedd4e722be75fc
SwedishMutantWerewolf <amber@bertbullough.com>
2018-09-02 12:03:59 -0600

    Initial Commit

---
+++ Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watch
+//
+//  Created by Amber Bullough on 9/2/18.
+//  Copyright © 2018 Amber Bullough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
